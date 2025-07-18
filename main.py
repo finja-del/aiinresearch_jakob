48251b000f7753c3ee7863af3ee3bf664dda9933
niklaspriensitz <uhohg@student.kit.edu>
2025-07-19 01:32:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from backend.controller.SearchController import router as search_router
 from backend.controller.ExportController import router as export_router
 
 app = FastAPI()
+application = app  # 👈 Wichtig für AWS Beanstalk
 #komentar zum pushen
 
 # Mount frontend static files
@@ -25,7 +26,4 @@ app.include_router(search_router, prefix="/api")
 # Include export API routes
 app.include_router(export_router, prefix="/api")
 
-# For local testing
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
